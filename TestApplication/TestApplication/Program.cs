596d4d21bfe0a979dcec8c300150a31c657349a6
dariusz.kostrzewa <dariusz.kostrzewa@sysrepublic.com>
2016-07-04 16:41:34 +0100

    Branch comment

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -14,6 +14,7 @@ namespace TestApplication
             // Some comment
             // Yet another one
             // My Another one
+            // Branched changes
             Console.ReadLine();
         }
     }
